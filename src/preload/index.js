9aa3d4b9cbac504efd04098d88c6a5d0c112c998
JeanCarlos Jimenez <jeancarlosjimenezc25@gmail.com>
2024-06-13 18:48:59 -0500

    Changes - Linux

--- src/preload/index.js
+++ src/preload/index.js
@@ -1,10 +1,10 @@
 import { contextBridge } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
 
-// Custom APIs for renderer
+//* Custom APIs for renderer
 const api = {}
 
-// Use `contextBridge` APIs to expose Electron APIs to
+// *Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
 // just add to the DOM global.
 if (process.contextIsolated) {
